96f0bd160274e7108e7f151d8efc60a1b7d2fa3c
theroyakash <royakashappleid@icloud.com>
2024-01-26 23:32:49 +0530

    update to the testing strategy

--- RandomGraphs/tests/test_utils.h
+++ RandomGraphs/tests/test_utils.h
@@ -2,19 +2,19 @@
 #define TEST_UTILS
 
 #include <cassert>
+#include <iomanip>
 #include <iostream>
 #include <string>
-#include <iomanip>
 
-void printOk() {
+void printPassing() {
     std::cout << "\033[32m[ passed ]\033[0m" << std::endl;
 }
 
-void printNotOK() {
+void printNotPassing() {
     std::cout << "\033[31m[ failed ]\033[0m" << std::endl;
 }
 
-void printBox(const std::string& message) {
+void printBox(const std::string &message) {
     int width = message.length() + 2;
 
     // Top border
@@ -26,7 +26,6 @@ void printBox(const std::string& message) {
     // Bottom border
     std::cout << '+' << std::string(width, '-') << '+' << std::endl;
 }
-
 
 namespace TestAssertService {
 void setUp(std::string fn_name) {
@@ -42,74 +41,92 @@ void cleanUp(std::string fn_name) {
 template <typename T>
 static void assertEqual(T actual, T expected, const std::string &testName) {
     std::cout << std::left << std::setw(30) << testName;
-    if (actual == expected) printOk();
-    else printNotOK();
+    if (actual == expected)
+        printPassing();
+    else
+        printNotPassing();
 }
 
 template <typename T>
 static void assertNotEqual(T actual, T expected, const std::string &testName) {
     std::cout << std::left << std::setw(30) << testName;
-    if (actual != expected) printOk();
-    else printNotOK();
+    if (actual != expected)
+        printPassing();
+    else
+        printNotPassing();
 }
 
 template <typename T>
 static void assertLessThan(T actual, T expected, const std::string &testName) {
     std::cout << std::left << std::setw(30) << testName;
-    if (actual < expected) printOk();
-    else printNotOK();
+    if (actual < expected)
+        printPassing();
+    else
+        printNotPassing();
 }
 
 template <typename T>
 static void assertGreaterThan(T actual, T expected, const std::string &testName) {
     std::cout << std::left << std::setw(30) << testName;
-    if (actual > expected) printOk();
-    else printNotOK();
+    if (actual > expected)
+        printPassing();
+    else
+        printNotPassing();
 }
 
 template <typename T>
 static void assertLessThanOrEqual(T actual, T expected, const std::string &testName) {
     std::cout << std::left << std::setw(30) << testName;
-    if (actual <= expected) printOk();
-    else printNotOK();
+    if (actual <= expected)
+        printPassing();
+    else
+        printNotPassing();
 }
 
 template <typename T>
 static void assertGreaterThanOrEqual(T actual, T expected, const std::string &testName) {
     std::cout << std::left << std::setw(30) << testName;
-    if (actual >= expected) printOk();
-    else printNotOK();
+    if (actual >= expected)
+        printPassing();
+    else
+        printNotPassing();
 }
 
 template <typename T>
 static void assertNull(T actual, const std::string &testName) {
     std::cout << std::left << std::setw(30) << testName;
-    if (actual == nullptr) printOk();
-    else printNotOK();
+    if (actual == nullptr)
+        printPassing();
+    else
+        printNotPassing();
 }
 
 template <typename T>
 static void assertNotNull(T actual, const std::string &testName) {
     std::cout << std::left << std::setw(30) << testName;
-    if (actual != nullptr) printOk();
-    else printNotOK();
+    if (actual != nullptr)
+        printPassing();
+    else
+        printNotPassing();
 }
 
 template <typename T>
 static void assertTrue(T condition, const std::string &testName) {
     std::cout << std::left << std::setw(30) << testName;
-    if (condition) printOk();
-    else printNotOK();
+    if (condition)
+        printPassing();
+    else
+        printNotPassing();
 }
 
 template <typename T>
 static void assertFalse(T condition, const std::string &testName) {
     std::cout << std::left << std::setw(30) << testName;
-    if (!condition) printOk();
-    else printNotOK();
+    if (!condition)
+        printPassing();
+    else
+        printNotPassing();
 }
-
-
 }  // namespace TestAssertService
 
 #endif  // TEST_UTILS
